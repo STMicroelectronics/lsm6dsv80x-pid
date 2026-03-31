1dc08a855f18c52bd09b9bc56a61b1db30bd0c2c
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2026-03-31 14:35:13 +0200

    Fix xl_setup API: remove dead code
    Removed duplicate condition that cannot be reached due to previous
    checks.
    
    Signed-off-by: Gioele Fiorenza <gioele.fiorenza2000@gmail.com>

--- lsm6dsv80x_reg.c
+++ lsm6dsv80x_reg.c
@@ -683,13 +683,6 @@ int32_t lsm6dsv80x_xl_setup(
     goto exit;
   }
 
-  // if odr is choosed as an high-accuracy value, mode should be set in high-accuracy
-  if ((xl_ha != 0u && xl_mode != LSM6DSV80X_XL_HIGH_ACCURACY_ODR_MD))
-  {
-    ret = -1;
-    goto exit;
-  }
-
   // Switching (enable/disable) HAODR mode require that all sensors must be in power-down mode.
   // Note: if both sensors are ON, lsm6dsv80x_haodr_set function must be used.
   if (haodr.haodr_sel != xl_ha &&
