3f6e3ff4ba93b929513fa12986bc292a601f2351
Armando Visconti <armando.visconti@st.com>
2026-05-26 14:25:32 +0200

    Fix wrong mask for tgt_add (7-bit i2c addr)
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- lsm6dsv80x_reg.c
+++ lsm6dsv80x_reg.c
@@ -7701,7 +7701,7 @@ int32_t lsm6dsv80x_sh_cfg_write(const stmdev_ctx_t *ctx,
     goto exit;
   }
 
-  reg.target0_add = (val->tgt0_add >> 1) & 0x07U;
+  reg.target0_add = (val->tgt0_add >> 1) & 0x7FU;
   reg.rw_0 = 0;
   ret = lsm6dsv80x_write_reg(ctx, LSM6DSV80X_TGT0_ADD, (uint8_t *)&reg, 1);
   if (ret != 0)
@@ -7819,7 +7819,7 @@ int32_t lsm6dsv80x_sh_tgt_cfg_read(const stmdev_ctx_t *ctx, uint8_t idx,
     return ret;
   }
 
-  tgt_add.target0_add = (val->tgt_add >> 1) & 0x07U;
+  tgt_add.target0_add = (val->tgt_add >> 1) & 0x7FU;
   tgt_add.rw_0 = 1;
   ret = lsm6dsv80x_write_reg(ctx, LSM6DSV80X_TGT0_ADD + (uint8_t)(idx * 3U),
                              (uint8_t *)&tgt_add, 1);
